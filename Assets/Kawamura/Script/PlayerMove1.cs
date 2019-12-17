5bbfb8491ef9220695c46b3fd8371b39a43108f8
tatu1105 <tattun1105@gmail.com>
2019-12-17 17:09:06 +0900

    プレハブの隙間をなくして道を一直線に変更しました。

--- Assets/Kawamura/Script/PlayerMove1.cs
+++ Assets/Kawamura/Script/PlayerMove1.cs
@@ -1,8 +1,4 @@
-﻿/*
- 2019/12/16
- 作成：川崎大樹
- */
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +7,8 @@ public class PlayerMove1 : MonoBehaviour
     [SerializeField]
     private float MaxSpeed;
     Rigidbody _rg;
-    public int _MoveCount_x = 0; //プレイヤーの横移動制限
+    public int _MoveCount_x { get; private set; }
+    //プレイヤーの横移動制限
 
 
     // Start is called before the first frame update
@@ -24,7 +21,7 @@ public class PlayerMove1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_rg.velocity.magnitude < MaxSpeed )
+    if (_rg.velocity.magnitude < MaxSpeed)
         {
             _rg.AddForce(transform.forward * speed);
         }
@@ -33,18 +30,6 @@ public class PlayerMove1 : MonoBehaviour
             _rg.velocity = transform.forward * 10;
         }
 
-        if (Input.GetKeyDown(KeyCode.A) && (_MoveCount_x < 1))//左移動
-        {
-            _MoveCount_x++;
-            transform.position -= transform.right;
-            
-        }
-        if (Input.GetKeyDown(KeyCode.D) && (_MoveCount_x > -1))
-        {
-            _MoveCount_x--;
-            transform.position += transform.right;
-            
-        }
     }
     private void OnTriggerStay(Collider other)
     {
