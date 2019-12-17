5fa578b331f8ed9b9ea9e5b082635171e4982ce4
KawasakiDaiki <45646482+KawasakiDaiki@users.noreply.github.com>
2019-12-17 14:52:53 +0900

    シーンセーブ忘れ

--- Assets/kawasaki/Scrips/PlayerMove.cs
+++ Assets/kawasaki/Scrips/PlayerMove.cs
@@ -29,7 +29,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //PlayerTurn();
+        PlayerTurn();
     }
     void FixedUpdate()
     {
