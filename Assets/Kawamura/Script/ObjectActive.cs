05362c81de7935e92facde778a75b8dbb50c1792
tatu1105 <tattun1105@gmail.com>
2019-12-18 06:57:17 +0900

    タイトル、チュートリアル、リザルトパネルの追加

---
+++ Assets/Kawamura/Script/ObjectActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectActive : MonoBehaviour
+{
+    public GameObject _Object0;
+    public GameObject _Object1;
+    private bool _Repeated = true;
+
+    public void ActiveObject()
+    {
+        if (_Repeated)
+        {
+            _Object0.SetActive(true);
+            _Object1.SetActive(true);
+            _Repeated = false;
+        }
+    }
+}
