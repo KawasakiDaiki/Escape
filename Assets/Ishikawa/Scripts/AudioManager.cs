e36932caa7658d3eaa25e6559ac386591a70e2a6
eleanor182 <in.eleanor182@gmail.com>
2019-12-19 11:10:26 +0900

    オーディオファイルを追加

---
+++ Assets/Ishikawa/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    [SerializeField] AudioSource BGMSource;
+    [SerializeField] AudioSource SESource;
+
+    [SerializeField] AudioClip[] SEClips;
+
+    enum SEClipName
+    {
+    }
+    void Awake()
+    {
+        CheckInstance();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+    }
+
+    public void PlaySE()
+    {
+        SESource.PlayOneShot(SESource.clip);
+    }
+}
