8c02570a790fe91296a0a63baca39037fa3a51b9
eleanor182 <in.eleanor182@gmail.com>
2019-12-19 11:24:54 +0900

    Android用の適用とdamageEffect仮実装

---
+++ Assets/Ishikawa/Scripts/DamageEffector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DamageEffector : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume damageEffectVolume;
+    [SerializeField] AnimationCurve effectCurve;
+    [SerializeField, Tooltip("エフェクトにかける時間")] float effectTime;
+
+    /// <summary>
+    /// ダメージエフェクトを再生する
+    /// </summary>
+    public void PlayEffect()
+    {
+        StartCoroutine(EffectCoroutine());
+    }
+
+    IEnumerator EffectCoroutine()
+    {
+        float time = 0;
+        while (true)
+        {
+            damageEffectVolume.weight = effectCurve.Evaluate(time / effectTime);
+            if (time > effectTime)
+            {
+                break;
+            }
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
